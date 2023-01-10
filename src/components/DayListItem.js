0bc8e478bd0c796b9e0d436fa7693e8d0c50e1a9
t-iffany <tiffanyxleong@gmail.com>
2023-01-10 13:57:36 -0800

    Create formatSpots function that will format props.spots to make tests pass

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,12 +10,26 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
 
+  const formatSpots = function() {
+    const spots = props.spots;
+    
+    if (spots === 0) {
+      return "no spots remaining";
+    }
+
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${spots} spots remaining`;
+  };
+
   return (
-    <li 
+    <li
       className={dayListItemClass}
-      onClick = {() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
