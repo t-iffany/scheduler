db70eeefbb2290f32b20c4b29b6a4a072d495d22
t-iffany <tiffanyxleong@gmail.com>
2023-01-10 13:36:11 -0800

    -Create scss file to hold styles for DayList Item -Update component to use classnames library to add styles dynamically based on whether DayListItem is selected or not

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+
+  const dayListItemClass = classNames("dayListItem", {
+    "day-list__item": true,
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
   return (
-    <li onClick = {() => props.setDay(props.name)}>
+    <li 
+      className={dayListItemClass}
+      onClick = {() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
