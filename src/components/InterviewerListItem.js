95994b5f65548adff32f4a0930a1a70f561460fb
t-iffany <tiffanyxleong@gmail.com>
2023-01-10 17:58:47 -0800

    Build <InterviewerListItem> component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+
+  const interviewerListItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    <li 
+      className={interviewerListItemClass}
+      onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
