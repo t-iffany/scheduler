e457aa9bf3ba7dad7c66326835387cdd7acf05e6
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 15:23:13 -0800

    -Refactor to add props and state -Add reset and cancel functions -Add onSubmit preventDefault event handler to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,13 +7,26 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = function() {
+    props.onCancel();
+    reset();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+          autoComplete="off"
+          onSubmit={event => event.preventDefault()}
+        >
           <input
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name={props.name}
             type="text"
             placeholder="Enter Student Name"
           /*
@@ -31,10 +44,11 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
   );
+  
 }
