2d5eab94d92ce4827936090155d06208dcd0d9ed
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 12:40:49 -0800

    Build <Empty> component, add story, refactor component to use props

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
+import Empty from "components/Appointment/Empty.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -146,4 +147,5 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm" />);
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
