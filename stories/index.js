069d412b450d1f4c44dd908023522e4559561a62
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 13:19:10 -0800

    Build <Error> component, add story, refactor component to use props

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty.js";
 import Show from "components/Appointment/Show.js";
 import Confirm from "components/Appointment/Confirm.js";
 import Status from "components/Appointment/Status.js";
+import Error from "components/Appointment/Error.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -167,4 +168,10 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
-  .add("Status", () => <Status message="Deleting" />);
+  .add("Status", () => <Status message="Deleting" />)
+  .add("Error", () => (
+    <Error
+      message="Could not delete appointment."
+      onClose={action("onClose")}
+    />
+  ));
