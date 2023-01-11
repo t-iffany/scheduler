3c7b439c2d7e3ee07d41fa7ef108483f05632a26
t-iffany <tiffanyxleong@gmail.com>
2023-01-10 19:26:40 -0800

    Update DayListItem return to include props.selected

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -26,7 +26,8 @@ export default function DayListItem(props) {
   return (
     <li
       className={dayListItemClass}
-      onClick={() => props.setDay(props.name)}>
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
