139a1032ccde5c2bd38d878dfe5283495383e3f8
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 15:38:44 -0800

    Display Appointment Empty and update <Appointment> component to add ternary operatory conditional to render <Show> or <Empty>

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+    </article>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -191,4 +191,10 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
