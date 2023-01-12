b15a352df4b5173d194790fe5e372230f06f0972
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 16:10:29 -0800

    -Add mock {appointments} data -Import <Appointment> component into Application.js -In Schedule section, convert {appointments} obj to an array and map over array to create list of <Appointment> components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import "components/Appointment";
+import Appointment from "components/Appointment";
+
 
 const days = [
   {
@@ -20,6 +23,47 @@ const days = [
   },
 ];
 
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
@@ -27,27 +71,38 @@ export default function Application(props) {
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-        className="sidebar--centered"
-        src="images/logo.png"
-        alt="Interview Scheduler"
-      />
-      <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu">
-        <DayList
-          days={days}
-          value={day}
-          onChange={setDay}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
         />
-      </nav>
-      <img
-        className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
-        alt="Lighthouse Labs"
-      />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            value={day}
+            onChange={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
+        {Object.values(appointments).map(appointment => {
+          return (
+            <Appointment
+              key={appointment.id}
+              id={appointment.id}
+              time={appointment.time}
+              interview={appointment.interview}
+            />
+          );
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
