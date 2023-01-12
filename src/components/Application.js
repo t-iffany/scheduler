d8713690d7cd3a0461dd3eb1cc9c3386b51e0c5b
t-iffany <tiffanyxleong@gmail.com>
2023-01-11 20:49:09 -0800

    -Import axios and useEffect in Application.js -Remove days array, use useState to add a days state to <Application> component -Make useEffect to make an axios.get request and update the days state with the response

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment";
 import Appointment from "components/Appointment";
-
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 
 const appointments = {
@@ -66,7 +48,14 @@ const appointments = {
 
 export default function Application(props) {
 
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:8001/api/days")
+      .then(response => {
+      setDays([...response.data]);
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -80,8 +69,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day}
-            onChange={setDay}
+            value={days}
+            onChange={setDays}
           />
         </nav>
         <img
