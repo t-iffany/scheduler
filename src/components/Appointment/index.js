20be1bcdd5798638be10f4999419f64bd0b45a16
t-iffany <tiffanyxleong@gmail.com>
2023-01-13 00:22:39 -0800

    -Use useVisualMode custom hook -Conditionally render Appointment component -Add CREATE transition and render Form component -Implement BACK function to return to EMPTY state when click onCancel

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,13 +3,40 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
+
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE ="CREATE";
+
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+      />
+      )}
+      {mode === CREATE &&
+        <Form
+          name={props.name}
+          value={props.value}
+          interviewers={[]}
+          onCancel={back}
+        />
+      }
     </article>
   );
 }
+
+
