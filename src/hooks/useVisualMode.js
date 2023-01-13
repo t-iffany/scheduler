6718309506d70702e2d23e9f00fad909ae51cdd7
t-iffany <tiffanyxleong@gmail.com>
2023-01-12 23:49:52 -0800

    -Build a Hook -Create transition and back functions -Add tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,29 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
   
-  return {mode}
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  // when transition is called, we need to add the new mode to our history
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      setMode(newMode);
+      history.push(newMode);
+    }
+    setMode(newMode);
+  }
+
+  // when back is called, we should set the mode to the previous item in our history array
+  // history.length must be greater than 1 because it should not allow user to go back past the initial mode
+  function back() {
+    if (history.length > 1) {
+      // remove the lement at the top of the stack
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+
 }
