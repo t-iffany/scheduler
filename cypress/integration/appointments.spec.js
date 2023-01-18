624e22897e5c221c85031b62f1fa47d00b619a05
t-iffany <tiffanyxleong@gmail.com>
2023-01-18 15:51:06 -0800

    Add appointment test for it(should edit an interview)

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -13,7 +13,7 @@ describe("Appointments", () => {
   it("should book an interview", () => {
 
     cy.get("[alt=Add]")
-      .first()  
+      .first()
       .click();
 
     cy.get("[data-testid=student-name-input]")
@@ -27,7 +27,28 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
-      
+
+  });
+
+  it("should edit an interview", () => {
+
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
+    cy.contains("Save")
+      .click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+
   });
 
 });
