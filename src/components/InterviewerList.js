47b5385682ee47f733119aa3955b09b57a279fe3
t-iffany <tiffanyxleong@gmail.com>
2023-01-17 16:14:28 -0800

    -Install and import prop-types library -Validate interviewers prop is an Array in InterviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "components/InterviewerList.scss"
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from "prop-types";
 
 export default function InterviewerList(props) {
   
-  const interviewerList = props.interviewers.map(interviewer => {
+  const interviewerList = 
+    Array.isArray(props.interviewers) &&
+    props.interviewers.map(interviewer => {
     return (
       <InterviewerListItem
         key={interviewer.id}
@@ -16,10 +19,15 @@ export default function InterviewerList(props) {
     );
   })
 
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{interviewerList}</ul>
     </section>
   );
+
 }
