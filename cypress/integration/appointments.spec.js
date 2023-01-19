62f7f4d996e940af4a1bdb0fd6027e9c3c41c373
t-iffany <tiffanyxleong@gmail.com>
2023-01-18 16:06:00 -0800

    Add appointment test for it(should delete an interview)

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,7 +1,6 @@
 
 describe("Appointments", () => {
 
-  // separate common test commands
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
 
@@ -48,6 +47,22 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  });
+
+  it("should delete an interview", () => {
+
+    cy.get("[alt=Delete]")
+      .click({ force: true });
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
 
   });
 
